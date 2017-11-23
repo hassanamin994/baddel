807e494b1877ab2e99a27a5a42aec16323e9253c
Hassan Amin <hassan94924@gmail.com>
2017-11-23 21:50:47 +0200

    Switch from superagent to axios

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,8 +4,7 @@ const validator = require('validator');
 const jwt = require('jwt-simple');
 const config = require('../config');
 const passport = require('passport');
-const superagent = require('superagent');
-
+const axios = require('axios');
 
 const authSignin = passport.authenticate('local', {session: false});
 
@@ -49,11 +48,10 @@ router.post('/signup', (req, res, next) => {
 
 router.post('/facebook', (req, res) => {
     const { accessToken } = req.body;
-    superagent.get(`${FACEBOOK_GRAPH_URL}&access_token=${accessToken}`)
-    .end((err, userInfo) => {
-        if(err) return res.status(422).send('Something went wrong, please try again later!');
-
-        const userData = JSON.parse(userInfo.text);
+    axios.get(`${FACEBOOK_GRAPH_URL}&access_token=${accessToken}`)
+    .then( userInfo => {
+        console.log(userInfo)
+        const userData = userInfo.data;
         User.findOne({ facebook_id: userData.id  })
         .then(user => {
             if(user) {
@@ -75,7 +73,11 @@ router.post('/facebook', (req, res) => {
         .catch(err => {
             res.status(422).send('Something went wrong! Please try again later')
         })
-    });
+    })
+    .catch(err => {
+        console.log(err)
+         return res.status(422).send('Something went wrong, please try again later!');
+    })
     
     // res.status(422).send('Something went wrong')
 });
