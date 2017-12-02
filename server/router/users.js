1310df316793deb4f556dfb9f30d095a8c8831c6
Hassan Amin <hassan94924@gmail.com>
2017-12-02 19:16:58 +0200

    Enable route for user to get his info by access token

--- server/router/users.js
+++ server/router/users.js
@@ -1,5 +1,11 @@
 const router = require('express').Router() ;
+const passport = require('passport');
+const requireAuth = passport.authenticate('jwt', {session: false});
 
+
+router.get('/me', requireAuth, (req, res) => {
+    return res.json({user: req.user});
+})
 
 
 
